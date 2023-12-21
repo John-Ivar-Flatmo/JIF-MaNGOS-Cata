321e092fe1187f9fdb6aac45918766f461824ace
John Ivar Flatmo <JIFlatmo@mail.com>
2023-12-21 23:30:03 +0100

    fix scripts
    Signed-off-by: John Ivar Flatmo <JIFlatmo@mail.com>

--- fullCompile.sh
+++ fullCompile.sh
@@ -23,7 +23,7 @@ echo "default-libmysqlclient-dev libace-dev"
 export CC=/usr/bin/clang	##mangos seems to prefer clang for building, fine
 export CXX=/usr/bin/clang++
 
-cmakeFlags="-DCMAKE_INSTALL_PREFIX=$buildDir/build/built -DBUILD_MANGOSD:BOOL=1 -DBUILD_REALMD:BOOL=1 -DBUILD_TOOLS:BOOL=0 -DUSE_STORMLIB:BOOL=1 -DSCRIPT_LIB_ELUNA:BOOL=1 -DSCRIPT_LIB_SD3:BOOL=1 -DSOAP:BOOL=1 -DPLAYERBOTS:BOOL=1 -DPCH:BOOL=1 -DDEBUG:BOOL=1"
+cmakeFlags="-DCMAKE_INSTALL_PREFIX=$scriptDir/build/built -DBUILD_MANGOSD:BOOL=1 -DBUILD_REALMD:BOOL=1 -DBUILD_TOOLS:BOOL=0 -DUSE_STORMLIB:BOOL=1 -DSCRIPT_LIB_ELUNA:BOOL=1 -DSCRIPT_LIB_SD3:BOOL=1 -DSOAP:BOOL=1 -DPLAYERBOTS:BOOL=1 -DPCH:BOOL=1 -DDEBUG:BOOL=1"
 # wont build without PCH enabled atm	##tools broke atm
 mkdir "$scriptDir/build"
 mkdir "$scriptDir/build/built"
